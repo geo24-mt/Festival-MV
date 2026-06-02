fa6dbb52825727d41d14c98cb3be94dc89e47711
geo24-mt <geo24_mt@web.de>
2026-06-02 14:08:33 +0200

    Add files via upload

---
+++ vorarbeit/js/festival-seiten.js
@@ -0,0 +1,22 @@
+// festival-seiten.js
+const festivalLinks = {
+    "Air Beat One Festival": "https://airbeat-one.de/",
+    "Blechsonne Festival": "https://blechsonne.org/de",
+    "Doomstone Open Air": "https://rausgegangen.de/events/doomstone-open-air-0/",
+    "Fusion Festival": "https://fusion-festival.de/de",
+    "Immergut Festival": "https://immergutrocken.de/",
+    "Indian Spirit Festival": "https://www.indian-spirit.de/",
+    "Juroto Festival": "https://www.juroto.de/",
+    "Musik & Stille Festival": "https://musikundstille.com/",
+    "Ohne Watt Festival": "https://winkelleu.de/ohnewatt-2026/",
+    "Orange Sun Festival": "https://orangesunfestival.ebtix.de/event/K3HUJP/page",
+    "Pangea Festival": "https://www.aboutyoupangea-festival.de/",
+    "Praerie Festival": "https://praerie-festival.com/",
+    "Summer Dream Festival": "https://www.summerdream-festival.com/",
+    "Tag am Meer Festival": "https://www.festivalsunited.com/festivals/tag-am-meer-festival",
+    "Zappanale Festival": "https://zappanale.de/de/",
+	"Biolumina Festival":"https://biolumina.weticket.io/biolumina-1?fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGnvBlL6E6XfjVObbr7LBT_9BLYWpbjKMvwwdhOFLP_RQhwU4Xry4cEDA38vX8_aem_QXMudEbgh6R8tS5V-19euA",
+	"Piersession Binz":"https://binzer-bucht.de/veranstaltungen/piersession/",
+	"Transit Festival":"https://transit-festival.de/",
+	"3000 Grad Festival":"https://3000-festival.de/"
+};
